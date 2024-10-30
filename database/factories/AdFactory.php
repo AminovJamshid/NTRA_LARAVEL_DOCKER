cf6902db0261ddc457e2c4b9c518cff836748dae
AminovJamshid <aminovjamshid777@gmail.com>
2024-10-30 12:22:37 +0500

    PATCH: add AdFactory

--- database/factories/AdFactory.php
+++ database/factories/AdFactory.php
@@ -1,2 +1,32 @@
 <?php
 
+namespace Database\Factories;
+
+use App\Models\Branch;
+use App\Models\Status;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/* @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ad>
+ */
+class AdFactory extends Factory
+{
+/*Define the models default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->numberBetween(10000, 100000), // Butun sonlar
+            'rooms' => $this->faker->randomDigit(),
+            'address' => $this->faker->address(),
+            'gender' => $this->faker->randomElement(['male', 'female']),
+            'user_id'=> User::factory(),
+            'branch_id'=> Branch::factory(),
+            'status_id'=> Status::factory(),
+        ];
+    }
+}
